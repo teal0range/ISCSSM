277118aafd8ba55d0dc76f1003372c658288be4b
GJJ <1466494272@qq.com>
2021-02-19 13:30:46 +0800

    sentiment preprocess

--- sentiment.py
+++ sentiment.py
@@ -1,27 +1,43 @@
 # t-1期6月到t期7月 匹配 t期到t+1期回报
 import pandas as pd
 from Io import *
+from settings import *
+import numpy as np
 
 io = CsvIo()
 
 
 def preprocess():
     QX_DIVIDENDPREMIUM = io.readData("QX_DIVIDENDPREMIUM")
-    QX_DIVIDENDPREMIUM = QX_DIVIDENDPREMIUM[['SgnYear', 'LogPDND']]
+    QX_DIVIDENDPREMIUM['SgnYear'] = QX_DIVIDENDPREMIUM['SgnYear'].astype(str)
+    QX_DIVIDENDPREMIUM = QX_DIVIDENDPREMIUM[['SgnYear', 'LogPDND']].set_index('SgnYear').sort_index()
+
     QX_FUNDDISCOUNTPREMIUM = io.readData("QX_FUNDDISCOUNTPREMIUM")
     QX_FUNDDISCOUNTPREMIUM['SgnYear'] = QX_FUNDDISCOUNTPREMIUM['TradingDate'].str[:4]
-    QX_FUNDDISCOUNTPREMIUM = QX_FUNDDISCOUNTPREMIUM.groupby("SgnYear").apply(lambda x: x['CovertRate'].mean())
-    QX_IPO = io.readData("QX_IPO.csv")
+    QX_FUNDDISCOUNTPREMIUM = QX_FUNDDISCOUNTPREMIUM.groupby("SgnYear").apply(lambda x: x['CovertRate'].mean()).\
+        sort_index().rename("FUNDDISCOUNTPREMIUM")
+
+    QX_IPO = io.readData("QX_IPO")
     QX_IPO['SgnYear'] = QX_IPO['ListedDate'].str[:4]
-    QX_IPORETURN = QX_IPO.groupby("SgnYear").apply(lambda x: x['ReturnRate'].mean())
-    QX_IPONUMBER = QX_IPO.groupby("SgnYear").apply(lambda x: x['ReturnRate'].count())
+    QX_IPORETURN = QX_IPO.groupby("SgnYear").apply(lambda x: x['ReturnRate'].mean()).sort_index().rename("IPORETURN")
+    QX_IPONUMBER = QX_IPO.groupby("SgnYear").apply(lambda x: len(x)).sort_index().rename("IPONUMBER")
 
     QX_STOCKRATE = io.readData("QX_STOCKRATE")
+    QX_STOCKRATE['SgnYear'] = QX_STOCKRATE['SgnYear'].astype(str)
+    QX_STOCKRATE = QX_STOCKRATE.set_index('SgnYear').sort_index()
 
     QX_TRM = io.readData("QX_TRM")
-    QX_TRM = QX_TRM[(QX_TRM['MarketType'] == 1) | (QX_TRM['MarketType'] == 3)]
+    QX_TRM = QX_TRM[(QX_TRM['MarketType'] == 1)]
     QX_TRM['SgnYear'] = QX_TRM['TradingDate'].str[:4]
+    QX_TRM = QX_TRM.groupby("SgnYear").apply(lambda x: np.log(x['TurnoverRate' + str(1 + 1 * TRADABLE_EQUITY)].sum())).\
+        rename("LogTurnoverRate")
+    return pd.concat([QX_DIVIDENDPREMIUM, QX_TRM, QX_IPONUMBER, QX_IPORETURN, QX_STOCKRATE, QX_FUNDDISCOUNTPREMIUM],
+                     axis=1).sort_index().dropna()
 
 
 def run():
     pass
+
+
+if __name__ == '__main__':
+    print(preprocess())
